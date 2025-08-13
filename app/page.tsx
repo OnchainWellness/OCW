cc87d6c5c6d76f87f8c46acac736fc6c7820a6b6
Emerson Warhman <edwarhman@gmail.com>
2025-08-13 17:53:25 -0400

    fix footer logo margin-top

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Home from './components/Home/Home';
 
 export default function App() {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen mb-36">
       <main className="">
         <Home />
       </main>
